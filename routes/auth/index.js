62cb62993180be64133ca77f86a73d923a0aed94
Allan James Lapid <ajLapid718@gmail.com>
2018-10-27 21:21:17 -0400

    feat (Auth): Authenticate user via email and password

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -17,4 +17,24 @@ router.post('/signup', async (req, res, next) => {
   }
 });
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findByEmail(req.body.email);
+    if (!user) {
+      console.log('No such user found:', req.body.email);
+      res.status(401).send('Wrong username and/or password');
+    }
+    else if (!user.isCorrectPassword(req.body.password)) {
+      console.log('Incorrect password for user:', req.body.email);
+      res.status(401).send('Wrong username and/or password');
+    }
+    else {
+      req.login(user, err => (err ? next(err) : res.json(user)));
+    }
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
