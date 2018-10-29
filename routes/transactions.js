3a7bbb3e46f84483b40433558c02ff95f5e17fd7
Allan James Lapid <ajLapid718@gmail.com>
2018-10-29 00:22:11 -0400

    feat (transactions): Set the parent of the transaction to the user

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { Transaction } = require('../database/models');
 
-router.post('/', function(req, res, next) {
-  Transaction.create(req.body)
-    .then(() => res.status(201).send("Created!"))
-    .catch(next)
+router.post('/', async (req, res, next) => {
+  const currentTransaction = await Transaction.build(req.body);
+  currentTransaction.setUser(req.user);
+  currentTransaction.save()
+    .then(() => res.status(201).send('Created!'))
+    .catch(err => next(err))
 });
 
 // Export our router, so that it can be imported to construct our apiRouter;
