251d02e23f01570bae38517dbd9c4ed44027a9ec
Allan James Lapid <ajLapid718@gmail.com>
2020-06-22 02:12:55 -0400

    domain

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const configureApp = () => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: true,
+        domain: "https://serene-varahamihira-fbc247.netlify.app"
       }
     })
   );
