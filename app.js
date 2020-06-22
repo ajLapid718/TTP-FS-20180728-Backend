2447c7370a4ccce8e51f470f3c1890f871abde2c
Allan James Lapid <ajLapid718@gmail.com>
2020-06-22 01:45:05 -0400

    cookie

--- app.js
+++ app.js
@@ -89,11 +89,11 @@ const configureApp = () => {
       store: sessionStore,
       resave: false,
       saveUninitialized: false,
-      cookie: {
-        secure: true,
-        sameSite: "strict",
-        httpOnly: true
-      }
+      // cookie: {
+      //   secure: true,
+      //   sameSite: "strict",
+      //   httpOnly: true
+      // }
     })
   );
 
